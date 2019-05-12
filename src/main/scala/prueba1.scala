b96ee35b8dba9532ad9d13c5279c25b47f9d1126
bmoral <beatrizmoralm@gmail.com>
2019-05-12 23:52:30 +0200

    Ejercicio 3 en desarrollo testeo funcion AñadirLibro Añadido Prueba1 para desarrollar el ultimo punto del ejercicio

--- src/main/scala/prueba1.scala
+++ src/main/scala/prueba1.scala
@@ -3,27 +3,28 @@ import scala.io.StdIn
 
 object prueba1 extends App {
   class Libreria ( libro: Libro) {
-    //var listaLibro  = listaLibro
-    println(listaLibro)
+    var listaLibro:List[Libro]= List(new Libro("libro1",150),new Libro("libro2", 600), new Libro("libro3", 500))
 
     def añadirLibro(libro: Libro) = {
       println("Estoy en el metodo con " + libro.titulo)
+      println(listaLibro.length.toString)
       for (x <- 0 until listaLibro.length) {
-        if (libro.titulo == listaLibro(x)) {
+        if (libro.titulo == listaLibro.) {
           println("Ya existe este libro.")
         }
       }
     }
-  }
+    }
 
-  override def main(args: Array[String]): Unit = {
-    println("Introduce el Libro:")
-    val libroUsuario : String = StdIn.readLine()
-    println("Introduce el número de páginas:")
-    val numPagUsuario : Int = StdIn.readInt()
-    var otherbook = new Libro(libroUsuario,numPagUsuario)
-    new Libreria(otherbook).añadirLibro(otherbook)
-  }
+    override def main(args: Array[String]): Unit = {
+
+      println("Introduce el Libro:")
+      val libroUsuario: String = StdIn.readLine()
+      println("Introduce el número de páginas:")
+      val numPagUsuario: Int = StdIn.readInt()
+      var otherbook = new Libro(libroUsuario, numPagUsuario)
+      new Libreria(otherbook).añadirLibro(otherbook)
+    }
 
 
 }
